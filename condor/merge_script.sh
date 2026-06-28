345d681e1a12cf06f4afe19f1036dc4873a83f28
JonaCassens <jonathancassens@gmail.com>
2026-06-28 13:45:42 +0100

    chore: Move obsolete condor submit files to archive and update .gitignore
    Moved legacy condor submit scripts and shell wrappers to condor/archive/,
    which is gitignored. Added condor/archive/ and notes sections to .gitignore.

--- condor/merge_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/hep/jcc525/gan_particle_physics
-source /home/hep/jcc525/gan_particle_physics/.venv/bin/activate
-
-python -u src/merge_batches.py "$@"
--- condor/preprocess_job_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# filepath: /home/hep/jcc525/gan_particle_physics/condor/preprocess_job_script.sh
-
-cd /home/hep/jcc525/gan_particle_physics
-source /home/hep/jcc525/gan_particle_physics/.venv/bin/activate
-
-python -u src/preprocess_rootfiles.py "$@"
